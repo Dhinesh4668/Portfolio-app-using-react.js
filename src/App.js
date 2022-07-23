77925b5ffbf7c7dd2885466d187ce4e3dcb83e18
dhinesh4668 <dhinesh4668@gmail.com>
2022-07-23 19:44:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+ import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
+    
   );
 }
 
